95ea9865259457c5a878a7f25941e340bdb1c74a
Luis cespedes <lxluxo23@gmail.com>
2023-01-05 11:07:48 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gustavo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
